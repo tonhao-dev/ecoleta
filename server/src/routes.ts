bcfceb2703212e1261cd03c5d363b0e871560c02
Luis Antonio <luis.developer.ac@gmail.com>
2020-07-25 20:39:26 -0500

    Servidor da aplicacao

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import PointsController from './controllers/pointsController'
+import ItemsController from './controllers/itemsController'
+
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+
+const routes = express.Router()
+
+routes.get('/items', itemsController.index)
+
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
+
+export default routes
