22e3477ccbc7ec6ce0bb3dc31af1e41d36b41745
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-07-30 18:15:11 -0500

    Integracao com a API do IBGE

--- web/src/pages/CreatePoint/createPoint.tsx
+++ web/src/pages/CreatePoint/createPoint.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import { Link } from 'react-router-dom'
 import { Map, TileLayer, Marker } from 'react-leaflet'
+import axios from 'axios'
 import api from '../../services/api'
 
 import { FiArrowLeft } from 'react-icons/fi'
@@ -13,8 +14,20 @@ interface Item{
     image_url: string
 }
 
+interface IbgeUfResponse {
+    sigla: string
+}
+
+interface IbgeCityResponse {
+    nome: string
+}
+
 const CreatePoint = () => {
     const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs] = useState<string[]>([])
+    const [selectedUf, setSelectedUf] = useState('0')
+    const [Cities, setCities] = useState<string[]>([])
+    const [selectedCity, setSelectedCity] = useState('0')
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -22,6 +35,36 @@ const CreatePoint = () => {
         })
     }, [])
 
+    useEffect(() => {
+        axios.get<IbgeUfResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response =>{
+            const ufInitials = response.data.map(uf => uf.sigla)
+
+            setUfs(ufInitials)
+        })
+    }, [])
+
+    useEffect(() => {
+        if(selectedUf === '0') {
+            return
+        }
+
+        axios.get<IbgeCityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then(response =>{
+            const cityNames = response.data.map(city => city.nome)
+
+            setCities(cityNames)
+        })
+    }, [selectedUf])
+
+    function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value
+        setSelectedUf(uf)
+    }
+
+    function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
+        const city = event.target.value
+        setSelectedCity(city)
+    }
 
     return (
         <div id="page-create-point">
@@ -90,15 +133,31 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado</label>
-                            <select name="uf" id="uf">
+                            <select 
+                                name="uf" 
+                                id="uf"
+                                value={selectedUf} 
+                                onChange={handleSelectedUf}
+                            >
                                 <option value="0">Selecione o estado</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
                             </select>
                         </div>
 
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select 
+                                name="city" 
+                                id="city" 
+                                value={selectedCity} 
+                                onChange={handleSelectedCity}
+                            >
                                 <option value="0">Selecione a cidade</option>
+                                {Cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
