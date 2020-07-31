780701020d1b935456f2953f22cf9c0c9b54fe77
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-07-31 18:04:59 -0500

    Configurando as rotas da aplicacao mobile

---
+++ mobile/src/pages/Detail/detail.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Detail = () => {
+    return (
+        <View>
+            <Text>
+                Tela detalhando as informações do ponto de coleta
+            </Text>
+        </View>
+    )
+}
+
+export default Detail
---
+++ mobile/src/pages/Points/points.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Points = () => {
+    return (
+        <View>
+            <Text>
+                Mapa mostrando os pontos de coleta
+            </Text>
+        </View>
+    )
+}
+
+export default Points
