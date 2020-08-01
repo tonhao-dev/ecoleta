6a3cb81c42328750058d057eda3a0e16684a2a11
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-07-31 19:22:29 -0500

    Tela de detalhamento de um ponto de coleta de residuos

--- mobile/src/pages/Detail/detail.tsx
+++ mobile/src/pages/Detail/detail.tsx
@@ -1,13 +1,48 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { RectButton } from 'react-native-gesture-handler'
 
 const Detail = () => {
+    const navigation = useNavigation()
+
+    function handleNavigateBack() {
+        navigation.goBack()
+    }
+
     return (
-        <View>
-            <Text>
-                Tela detalhando as informações do ponto de coleta
-            </Text>
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={handleNavigateBack}>
+                    <Icon name="arrow-left" size={20} color="#34cb79" />
+                </TouchableOpacity>
+
+                <Image style={styles.pointImage} source={{
+                    uri: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+                }}/>
+
+                <Text style={styles.pointName}>Minha casa</Text>
+                <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+
+                <View style={styles.address}>
+                    <Text style={styles.addressTitle}>Endereço</Text>
+                    <Text style={styles.addressContent}>Rio Branco, Ac</Text>
+                </View>
+            </View>
+
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={() => {}}>
+                    <FontAwesome name="whatsapp" size={20} color="#fff" />
+                    <Text style={styles.buttonText}>Whatsapp</Text>
+                </RectButton>
+
+                <RectButton style={styles.button} onPress={() => {}}>
+                    <Icon name="mail" size={20} color="#fff" />
+                    <Text style={styles.buttonText}>E-mail</Text>
+                </RectButton>
+            </View>
+        </SafeAreaView>
     )
 }
 
@@ -15,7 +50,7 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
-      paddingTop: 20,
+      paddingTop: 32,
     },
   
     pointImage: {
